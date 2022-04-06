d03bd6989d5dc58bfe92f1fd7c0219e59da0cca5
Hyeon Sang Lee <88080251+phainestha1@users.noreply.github.com>
2022-04-06 18:02:10 +0900

    FEAT: Create Sample Folders (models, controllers, and views) with some files"

---
+++ buy-or-not/views/Search.swift
@@ -0,0 +1,23 @@
+//
+//  Search.swift
+//  buy-or-not
+//
+//  Created by Noah's Ark on 2022/04/06.
+//
+
+import SwiftUI
+
+struct Search: View {
+    var body: some View {
+        VStack {
+            Title(text: "검색 화면")
+            Text("이곳에서 검색 화면을 만들겁니다.")
+        }
+    }
+}
+
+struct Search_Previews: PreviewProvider {
+    static var previews: some View {
+        Search()
+    }
+}
