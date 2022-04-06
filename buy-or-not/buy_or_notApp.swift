e84e1abca6985dffc339d03d68e2407d9caed70b
Hyeon Sang Lee <88080251+phainestha1@users.noreply.github.com>
2022-04-06 17:02:37 +0900

    .

---
+++ buy-or-not/buy_or_notApp.swift
@@ -0,0 +1,17 @@
+//
+//  buy_or_notApp.swift
+//  buy-or-not
+//
+//  Created by Noah's Ark on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct buy_or_notApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- controllers/README.md
+++
@@ -1,8 +0,0 @@
-# FinishLine-Buy-or-not
-FinishLine-Buy-or-not
-
-
-# 베테브가 클론함
-# 송쿨이 수정함
-# 소이가 수정함
-# 할로겐이 수정함
