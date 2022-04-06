4cb9c04c43cb920142067cb522be71045bae9ad9
bethev(leejunmo) <34985422+LEJMO@users.noreply.github.com>
2022-04-06 18:38:49 +0900

    메인파일 생성

---
+++ buy-or-not/views/Main.swift
@@ -0,0 +1,21 @@
+//
+//  Main.swift
+//  buy-or-not
+//
+//  Created by leejunmo on 2022/04/06.
+//
+// 메인페이지입니다.
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Main()
+    }
+}
