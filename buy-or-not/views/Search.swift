b0f17c1c31a68097051b6ccec4fc295f57cc8814
Hyeon Sang Lee <88080251+phainestha1@users.noreply.github.com>
2022-04-07 00:32:31 +0900

    FEAT: Set basic search form and feed styles

--- buy-or-not/views/Search.swift
+++ buy-or-not/views/Search.swift
@@ -1,6 +1,6 @@
 //
 //  Search.swift
-//  buy-or-not
+//  Buy or Not
 //
 //  Created by Noah's Ark on 2022/04/06.
 //
@@ -8,10 +8,66 @@
 import SwiftUI
 
 struct Search: View {
+    
+    @State var searchText: String = ""
+    @State var selectedPageIndex = 1
+    
     var body: some View {
         VStack {
-            Title(text: "검색 화면")
-            Text("이곳에서 검색 화면을 만들겁니다.")
+            // SearchBar and Picker Container
+            VStack {
+                VStack {
+                    // Searchbar Container
+                    VStack {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+                            TextField("검색", text: $searchText)
+                                .frame(width: 290)
+                                .background(Color(hex: "e9e9ea"))
+                            
+                            if (searchText != "") {
+                                Image(systemName: "xmark.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .onTapGesture {
+                                        searchText = ""
+                                    }
+                                }
+                                
+                            }
+                        }
+                }
+                .frame(width: 350, alignment: .leading)
+                .padding(.horizontal, 5)
+                .padding(.vertical, 9)
+                .background(Color(hex: "e9e9ea"))
+                .cornerRadius(10)
+        
+                // Tap bars in the upper section
+                Picker("Favorite Color", selection: $selectedPageIndex, content: {
+                    Text("답변 진행 중").tag(0)
+                    Text("모두 보기").tag(1)
+                    Text("답변 완료").tag(2)
+                })
+                .pickerStyle(SegmentedPickerStyle()) // <1>
+                .padding(.horizontal, 15)
+            }
+            
+            Spacer()
+            
+            // Picker will show different views based on its constant
+            if (selectedPageIndex == 0) {
+                Feed()
+            } else if (selectedPageIndex == 1) {
+                Feed2()
+            } else if (selectedPageIndex == 2) {
+                VStack {
+                    Feed()
+                    Feed2()
+                }
+
+            }
+        
         }
     }
 }
