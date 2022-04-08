db68976e44d6c70d5804167494e69c5aadaaaf79
Jiwon <zest1923@gmail.com>
2022-04-09 01:11:05 +0900

    질문페이지 수정

--- buy-or-not/Views/Question.swift
+++ buy-or-not/Views/Question.swift
@@ -17,10 +17,6 @@ struct Contents: View{
     @State private var price: String = ""
     @State private var link: String = ""
     
-    @State private var imagePickerPresented = false
-    @State private var selectedImage: UIImage?
-    @State private var profileImage: Image?
-    
     @State private var image = UIImage()
     @State private var showSheet = false
     
@@ -28,10 +24,9 @@ struct Contents: View{
     var body: some View{
         DisclosureGroup("고민항목 \(number + 1)", isExpanded: $topExpand[number]) {
             TextField("고민항목 이름", text: $name)
-            TextField("(선택) 가격을 입력해주세요", text: $price)
+            TextField("(선택) 가격을 입력해주세요", text: $price).keyboardType(.numberPad)
             TextField("(선택) 링크 추가", text: $link)
-            Image(uiImage: self.image)
-                .resizable()
+            Image(systemName: "camera.fill")
                 .frame(width: 100, height: 100)
                 .background(Color.black.opacity(0.2))
                 .aspectRatio(contentMode: .fill).onTapGesture {
@@ -50,77 +45,66 @@ struct Contents: View{
 struct Question: View {
     
     @State private var qCategory: String = "카테고리"
-    @State private var categoryExpand: Bool = false
-    @State private var Bools: [Bool] = [true, false]
-    
     @State private var qTitle: String = ""
-    
-    @State private var topExpand: [Bool] = [true, false, false]
-    @State private var addItem: [Bool] = [true, false, false, false]
-    @State private var name: [String] = ["", "", ""]
-    @State private var price: [String] = ["", "", ""]
-    @State private var link: [String] = ["", "", ""]
     @State private var content = ""
     
-    @State private var imagePickerPresented = [false, false, false]
-    
-    @State private var image = UIImage()
-    @State private var showSheet = false
-    
+    @State private var categoryExpand: Bool = false
+    @State private var Bools: [Bool] = [true, false]
+    @State private var addItem: [Bool] = [true, false, false, false]
     
     var body: some View {
-            VStack{
-                Form{
-                    TextField("글 제목", text: $qTitle)
-                    // 카테고리
-                    DisclosureGroup(qCategory, isExpanded: $categoryExpand) {
-                        List{
-                            ForEach(categorys, id: \.self){ category in
-                                Text(category).onTapGesture {
-                                    qCategory = category
-                                    categoryExpand = Bools[1]
-                                }.listRowBackground(self.qCategory == category ? Color.gray : Color(UIColor.white))
-                            }
+        VStack{
+            Form{
+                TextField("글 제목", text: $qTitle)
+                // 카테고리
+                DisclosureGroup(qCategory, isExpanded: $categoryExpand) {
+                    List{
+                        ForEach(categorys, id: \.self){ category in
+                            Text(category).onTapGesture {
+                                qCategory = category
+                                categoryExpand = Bools[1]
+                            }.listRowBackground(self.qCategory == category ? Color.gray : Color(UIColor.white))
                         }
                     }
-                    
-                    // 고민항목
-                    ForEach(0..<3){num in
-                        if addItem[num]{
-                            Contents(number: num).onTapGesture{addItem[num+1] = Bools[0]}
-                        }
-                    }
-                    
-                    // 고민내용
-                    ZStack(alignment: .topLeading) {
-                        RoundedRectangle(cornerRadius: 8, style: .continuous)
-                            .fill(Color(UIColor.secondarySystemBackground))
-                        
-                        if content.isEmpty {
-                            Text("고민내용")
-                                .foregroundColor(Color(UIColor.placeholderText))
-                                .padding(.horizontal, 8)
-                                .padding(.vertical, 12)
-                        }
-                        
-                        TextEditor(text: $content)
-                            .padding(4)
-                        
-                    }
-                    .frame(width: 300, height: 300)
                 }
                 
+                // 고민항목
+                ForEach(0..<3){num in
+                    if addItem[num]{
+                        Contents(number: num).onTapGesture{addItem[num+1] = Bools[0]}
+                    }
+                }
                 
-                Button("등록하기") {
-                    // action
-                }.foregroundColor(.white)
-                    .frame(width: 180, height: 42, alignment: .center)
-                    .background(.blue)
-                    .cornerRadius(5)
-                    .padding(.vertical, 24.0)
-            }.navigationBarTitle("질문하기")
+                // 고민내용
+                ZStack(alignment: .topLeading) {
+                    RoundedRectangle(cornerRadius: 8, style: .continuous)
+                        .fill(Color(UIColor.secondarySystemBackground))
+                    
+                    if content.isEmpty {
+                        Text("고민내용")
+                            .foregroundColor(Color(UIColor.placeholderText))
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 20)
+                    }
+                    
+                    TextEditor(text: $content)
+                        .padding(10)
+                    
+                }
+                .frame(width: 300, height: 300)
+            }
+            
+            
+            Button("등록하기") {
+                // action
+            }.foregroundColor(.white)
+                .frame(width: 180, height: 42, alignment: .center)
+                .background(.blue)
+                .cornerRadius(5)
+                .padding(.vertical, 24.0)
+        }.navigationBarTitle("질문하기")
     }
-
+    
 }
 
 //IMAGE PICKER
