c973f64f43bdfb97e81242a03a93ff107dbd0cdd
Jiwon <zest1923@gmail.com>
2022-04-10 12:15:17 +0900

    메인 카테고리 메뉴 접기/펼치기 수정완료

--- buy-or-not/Views/MainCategory.swift
+++ buy-or-not/Views/MainCategory.swift
@@ -11,10 +11,13 @@ import SwiftUI
 struct MainCategorys: View{
     let mainCategorys: [String] = [
         "모두보기", "for you", "패션/뷰티", "가구/인테리어", "식품/외식", "전자제품", "취미/여가", "기타"]
+    private var moreOrLess = ["chevron.down", "chevron.up"]
+    
     @State private var scrollers: Bool = true
     @State private var choiced: String = ""
+    @State private var index = 0
     
-    private var moreOrLess = "􀆈"
+    
     var body: some View{
         ZStack{
             HStack{
@@ -52,16 +55,22 @@ struct MainCategorys: View{
                 }
                 
                 Spacer()
-                //Text("􀆇").onTapGesture{scrollers.toggle()}
                 
-                Button("􀆇") {
+                //                Button(String(index)) {
+                //                    scrollers.toggle()
+                //                    index = (index+1)%1
+                //                }.foregroundColor(.white)
+                //                    .frame( height: 30, alignment: .center)
+                //                    .background(.gray)
+                //                    .cornerRadius(20)
+                //                    .padding(.vertical, 24.0)
+                //                    .buttonStyle(.bordered)
+                
+                Image(systemName: moreOrLess[index]).onTapGesture{
                     scrollers.toggle()
-                }.foregroundColor(.white)
-                    .frame( height: 30, alignment: .center)
-                    .background(.gray)
-                    .cornerRadius(20)
-                    .padding(.vertical, 24.0)
-                    .buttonStyle(.bordered)
+                    index = (index + 1) % 2
+                    print(index)
+                }
             }
         }
     }
